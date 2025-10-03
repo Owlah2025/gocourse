948ee15c8de91f8b1664bbd59bb191618f175fdf
Owlah2025 <205240681+Owlah2025@users.noreply.github.com>
2025-10-03 17:47:30 +0300

    removed hello.elf and add modified arrays.go

--- basics/arrays.go
+++ basics/arrays.go
@@ -14,10 +14,10 @@ func main() {
 
 	copiedArr:= myArr
 
-	for i,_ := range myArr{  //you can simplify: just remove the secod part _ and use i only
-		fmt.Println(i)
-	}
-	fmt.Println("***********************")
+	// for i,_ := range myArr{  //you can simplify: just remove the secod part _ and use i only
+	// 	fmt.Println(i)
+	// }
+	// fmt.Println("***********************")
 
 	for i:=0; i<len(copiedArr);i++ {
 		fmt.Println(copiedArr[i])
@@ -32,6 +32,24 @@ func main() {
 
 	a,b := aFunction()
 	fmt.Println("first number is:",a,"\nthe second number is:",b)
+
+y := 5		//if for some reason you're not going to use the y variable 
+_= y     // you can assign it to _ and ther won't be any problem 
+
+
+array1 := [3] int {1,2,3}
+array2 := [3] int {1,2,3}
+
+fmt.Println("is array1 equal to array 2:",array1 == array2)
+
+firstArary := [4]int {1,2,3,4}
+var secondArary  *[4]int = &firstArary
+secondArary[0]=11
+
+fmt.Println(firstArary,*secondArary)
+
+
+
 }
 
 //function to return 2 values 
